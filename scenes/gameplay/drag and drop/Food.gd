b2362ed0a6b872977bec615617153ad01516c7d2
D-Zofia <dajksler.zofia@gmail.com>
2026-02-12 19:32:59 +0100

    all good for now

--- scenes/gameplay/drag and drop/Food.gd
+++ scenes/gameplay/drag and drop/Food.gd
@@ -4,5 +4,8 @@ extends Node
 var Carrot=[2,1,Color(1.0,0.5,0.0), "Carrot", "res://scenes/gameplay/drag and drop/pics/carrot.png"]
 var MoldyCarrot=[-1,1,Color(0.71, 0.493, 0.063, 1.0), "Moldy Carrot", "res://scenes/gameplay/drag and drop/pics/moldy_carrot.png"]
 var Shroom=[1,1,Color(0.4, 0.234, 0.134, 1.0), "Mushroom", "res://scenes/gameplay/drag and drop/pics/shroom.png"]
+var Blue=[2,1,Color(0.0, 0.533, 0.94, 1.0), "Blue", "res://scenes/gameplay/drag and drop/pics/blue.png"]
 
-var FoodOwned=[Carrot,Carrot,MoldyCarrot,Carrot,MoldyCarrot,Carrot,MoldyCarrot]
+var FoodOwned=[Carrot,Carrot,MoldyCarrot,Carrot,MoldyCarrot,Carrot,MoldyCarrot,Blue,Blue,Blue,Blue]
+func RemoveItem(item)->void:
+	FoodOwned.remove_at(FoodOwned.rfind(item))
--- scenes/gameplay/drag and drop/gar.gd
+++ scenes/gameplay/drag and drop/gar.gd
@@ -1,11 +1,14 @@
 extends TextureRect
 
 var Capacity=5
-var EmptyPot=Color.BLACK
+var EmptyPotColor=Color.BLACK
+var EmptyPotPos=35.0
 
 var itemNumber
 var taste
 var nourishment
+var soupColor
+var soupPos
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,25 +18,42 @@ func newSoup()->void:
 	itemNumber=0
 	nourishment=0
 	taste=0
+	soupColor=EmptyPotColor
+	soupPos=EmptyPotPos
+	updateInfo()
 
 func updateInfo()->void:
-	$"../Staty/Capacity".text="Capacity: "
+	#wyświetlanie danych liczbowych
+	$"../Staty/Capacity".text=str("Capacity: ",itemNumber,"/",Capacity)
+	$"../Staty/Taste".text=str("Taste: ",taste)
+	$"../Staty/Nourishment".text=str("Nourishment: ",nourishment)
+	
+	#wygląd zupy w garze
+	find_child("soup").modulate=soupColor
+	find_child("soup").position.y=soupPos
 
 func _can_drop_data(_pos, data):
 	return data is Array
  
 func _drop_data(_pos, data):
-	if data[0].name=="ShroomPantry":
+	if data[0].texture!=null and itemNumber<Capacity:
+		if data[0].name!="ShroomPantry":
+			data[0].RemoveItem()
+			Food.RemoveItem(data[1])
+			print(Food.FoodOwned)
 		itemNumber+=1
 		taste+=data[1][0]
 		nourishment+=data[1][1]
-	elif data[0].texture!=null and itemNumber<Capacity:
-		data[0].RemoveItem()
-		itemNumber+=1
-		taste+=data[1][0]
-		nourishment+=data[1][1]
+		soupPos-=6
+		if itemNumber==0:
+			soupColor=data[1][2]
+		else:
+			soupColor=Color((soupColor.r*(itemNumber-1)+data[1][2].r)/itemNumber,(soupColor.g*(itemNumber-1)+data[1][2].g)/itemNumber,(soupColor.b*(itemNumber-1)+data[1][2].b)/itemNumber)
+		
+		updateInfo()
 	print("itemNumber: ", itemNumber,", taste: ",taste,", nourishment: ",nourishment)
 
 
 func _on_button_button_up() -> void:
 	newSoup()
+	Dialogic.start("test_timeline")
